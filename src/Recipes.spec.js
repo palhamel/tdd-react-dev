4dba7670de3d67d6189d219b6c89b613c9ad518f
Pål Hamel <pal.hamel@gmail.com>
2020-09-20 19:53:40 +0200

    setup tests with msw

--- src/Recipes.spec.js
+++ src/Recipes.spec.js
@@ -1,18 +1,41 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import { Recipes } from "./Recipes";
-import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 
-test("to render the heading, input field and button", () => {
-  render(<Recipes />);
+const allRecipes = [
+  { id: 1, title: "Burger" },
+  { id: 2, title: "French Toast" },
+  { id: 3, title: "Salmon" },
+];
 
-  //test 1:
-  expect(screen.getByRole("heading")).toHaveTextContent("Recipe Finder");
-  // test 2:
-  expect(
-    screen.getAllByPlaceholderText("Enter an ingredient to find recipes...")
-      .toBeIntheDocument
-  );
-  // test 3:
-  expect(screen.getByRole("button")).toHaveTextContent("Find");
-});
+const server = setupServer(
+  rest.get("/api/recipes", (req, res, ctx) => {
+    return res(ctx.json({ recipes: allRecipes }));
+  })
+);
+
+beforeAll(() => server.listen());
+afterAll(() => server.close());
+
+test('fetches and displays all recipes', async () => {
+  render(<Recipes />)
+
+  const listItems = await screen.findAllByRole('listitem');
+  expect(listItems)
+})
+
+// test("to render the heading, input field and button", () => {
+//   render(<Recipes />);
+
+//   //test 1:
+//   expect(screen.getByRole("heading")).toHaveTextContent("Recipe Finder");
+//   // test 2:
+//   expect(
+//     screen.getAllByPlaceholderText("Enter an ingredient to find recipes...")
+//       .toBeIntheDocument
+//   );
+//   // test 3:
+//   expect(screen.getByRole("button")).toHaveTextContent("Find");
+// });
