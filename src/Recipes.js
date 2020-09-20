2985ee992f3935295170c37107d252d4f8020381
Pål Hamel <pal.hamel@gmail.com>
2020-09-20 15:20:41 +0200

    add test of form and button

--- src/Recipes.js
+++ src/Recipes.js
@@ -4,6 +4,14 @@ export const Recipes = () => {
   return (
     <div>
       <h1>Recipe Finder</h1>
+      <form>
+        <input 
+        type="text"
+        name="ingredient"
+        placeholder="Enter an ingredient to find recipes..."
+        />
+        <button type="submit">Find</button>
+      </form>
     </div>
   );
 };
--- src/Recipes.spec.js
+++ src/Recipes.spec.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { Recipes } from './Recipes'
+import { Recipes } from "./Recipes";
+import { scryRenderedDOMComponentsWithClass } from "react-dom/test-utils";
 
-test("to render the heading", () => {
+test("to render the heading, input field and button", () => {
   render(<Recipes />);
 
-  expect(screen.getByRole('heading')).toHaveTextContent('Recipe Finder');
+  //test 1:
+  expect(screen.getByRole("heading")).toHaveTextContent("Recipe Finder");
+  // test 2:
+  expect(
+    screen.getAllByPlaceholderText("Enter an ingredient to find recipes...")
+      .toBeIntheDocument
+  );
+  // test 3:
+  expect(screen.getByRole("button")).toHaveTextContent("Find");
 });
