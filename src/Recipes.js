332c9b669f57817fad91cf45d422516daf7e8751
Pål Hamel <pal.hamel@gmail.com>
2020-09-22 15:26:15 +0200

    finish async test

--- src/Recipes.js
+++ src/Recipes.js
@@ -1,33 +1,38 @@
-import { json } from "msw/lib/types/context";
+// import { json } from "msw/lib/types/context";
 import React, { useEffect, useState } from "react";
 
-const Recipes = () => {
-  const [recipes, setRecipes] = useState([])
+export const Recipes = () => {
+  const [recipes, setRecipes] = useState([]);
 
   useEffect(() => {
     const fetchAllRecipes = async () => {
-      const response = await fetch('/api/recipes');
+      const response = await fetch("/api/recipes");
       if (response.ok) {
         const jsonResponse = await response.json();
         setRecipes(jsonResponse.recipes);
       }
-    }
+    };
     fetchAllRecipes();
-  })
-}
+  }, []);
 
-export const Recipes = () => {
   return (
     <div>
       <h1>Recipe Finder</h1>
       <form>
-        <input 
-        type="text"
-        name="ingredient"
-        placeholder="Enter an ingredient to find recipes..."
+        <input
+          type="text"
+          name="ingredient"
+          placeholder="Enter an ingredient to find recipes..."
         />
         <button type="submit">Find</button>
       </form>
+      <ul>
+        {recipes.map((recipe) => (
+          <li 
+            key={recipe.id}
+          >{recipe.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
