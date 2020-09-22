6d749b42f1c71d06bb58014f60e80d92367703a2
Pål Hamel <pal.hamel@gmail.com>
2020-09-22 16:18:46 +0200

    Simulating user interaction

--- src/Recipes.js
+++ src/Recipes.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from "react";
 
 export const Recipes = () => {
   const [recipes, setRecipes] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+  const [searchCriteria, setSearchCriteria] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const fetchAllRecipes = async () => {
@@ -15,20 +18,44 @@ export const Recipes = () => {
     fetchAllRecipes();
   }, []);
 
+  const handleOnInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const handleOnSubmitForm = (event) => {
+    event.preventDefault();
+    fetchRecipeByIngredient(inputValue);
+  };
+
+  const fetchRecipeByIngredient = async (ingredient) => {
+    const response = await fetch(`/api/recipes?ingredient=${ingredient}`);
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      setSearchCriteria(ingredient);
+      setRecipes(jsonResponse.recipes);
+    }
+  };
+
   return (
     <div>
       <h1>Recipe Finder</h1>
-      <form>
-        <input
-          type="text"
-          name="ingredient"
-          placeholder="Enter an ingredient to find recipes..."
+      <form onSubmit={handleOnSubmitForm}>
+        <input type="text" name="ingredient"
+               placeholder="Enter an ingredient to find recipes..."
+               value={inputValue}
+               onChange={handleOnInputChange}
         />
         <button type="submit">Find</button>
       </form>
+      {errorMessage && (
+        <p>{errorMessage}</p>
+      )}
+      {searchCriteria && (
+        <p>Showing results for {searchCriteria}:</p>
+      )}
       <ul>
         {recipes.map((recipe) => (
-          <li 
+          <li
             key={recipe.id}
           >{recipe.title}</li>
         ))}
--- src/Recipes.spec.js
+++ src/Recipes.spec.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { Recipes } from "./Recipes";
 import { rest } from "msw";
 import { setupServer } from "msw/node";
+
+// TESTS:
 
 const allRecipes = [
   { id: 1, title: "Burger" },
@@ -12,6 +14,9 @@ const allRecipes = [
 
 const server = setupServer(
   rest.get("/api/recipes", (req, res, ctx) => {
+    if (req.url.searchParams.get('ingredient') === 'fish') {
+      return res(ctx.json({ recipes: allRecipes.filter(recipe => recipe.id === 3) }));
+    }
     return res(ctx.json({ recipes: allRecipes }));
   })
 );
@@ -19,6 +24,24 @@ const server = setupServer(
 beforeAll(() => server.listen());
 afterAll(() => server.close());
 
+// Simulating user interaction:
+test('displays filtered recipes when searching for an ingredient', async () => {
+  render(<Recipes />);
+
+  const input = screen.getByPlaceholderText('Enter an ingredient to find recipes...');
+
+  fireEvent.change(input, { target: { value: 'fish' } });
+  fireEvent.click(screen.getByRole('button', { name: 'Find' }));
+
+  expect(await screen.findByText('Showing results for fish:'))
+    .toBeInTheDocument();
+
+  const listItems = screen.getAllByRole('listitem');
+  expect(listItems).toHaveLength(1);
+  expect(listItems[0]).toHaveTextContent('Salmon');
+});
+
+// Testing asynchronous code:
 test("fetches and displays all recipes", async () => {
   render(<Recipes />);
 
@@ -29,16 +52,17 @@ test("fetches and displays all recipes", async () => {
   expect(listItems[2]).toHaveTextContent("Salmon");
 });
 
-// test("to render the heading, input field and button", () => {
-//   render(<Recipes />);
+// TEST :
+test("to render the heading, input field and button", () => {
+  render(<Recipes />);
 
-//   //test 1:
-//   expect(screen.getByRole("heading")).toHaveTextContent("Recipe Finder");
-//   // test 2:
-//   expect(
-//     screen.getAllByPlaceholderText("Enter an ingredient to find recipes...")
-//       .toBeIntheDocument
-//   );
-//   // test 3:
-//   expect(screen.getByRole("button")).toHaveTextContent("Find");
-// });
+  //test 1:
+  expect(screen.getByRole("heading")).toHaveTextContent("Recipe Finder");
+  // test 2:
+  expect(
+    screen.getAllByPlaceholderText("Enter an ingredient to find recipes...")
+      .toBeIntheDocument
+  );
+  // test 3:
+  expect(screen.getByRole("button")).toHaveTextContent("Find");
+});
